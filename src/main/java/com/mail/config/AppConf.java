89b6e410165d7d5ad6f7d79d289f8e86d95773ae
elyamart <elyamanioussama3@gmail.com>
2022-08-21 12:51:09 +0100

    delete unused css files

--- src/main/java/com/mail/config/AppConf.java
+++ src/main/java/com/mail/config/AppConf.java
@@ -1,8 +1,8 @@
 package com.mail.config;
 
 public class AppConf {
-    public static String MAIL_HOST="192.168.1.24";
-    public static String DB_HOST="192.168.1.24";
+    public static String MAIL_HOST="192.168.0.135";
+    public static String DB_HOST="192.168.0.135";
     public static String DB_PORT="3306";
     public static String DB_NAME="fmail_db";
     public static String DB_USERNAME="fmail_user";
